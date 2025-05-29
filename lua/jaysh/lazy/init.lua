07b1139752185ed1b64ba9e520f5803c6c4bb027
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-05-29 11:56:38 +0300

    apparently adding a print for debugging fixes everything

--- lua/jaysh/lazy/init.lua
+++ lua/jaysh/lazy/init.lua
@@ -4,8 +4,14 @@ return {
             function()
                 if vim.b.copilot_enabled then
                     vim.cmd("Copilot disable")
+                    vim.b.copilot_enabled = false
+                    -- print("Copilot disabled")
+                    -- print("DEBUG: b.copilot_enabled = " .. tostring(vim.b.copilot_enabled))
                 else
                     vim.cmd("Copilot enable")
+                    vim.b.copilot_enabled = true
+                    -- print("Copilot enabled")
+                    -- print("DEBUG: b.copilot_enabled = " .. tostring(vim.b.copilot_enabled))
                 end
             end),
         {
