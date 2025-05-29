4d776be0c66708ce7805cfc6c9a3ce4b0f53a05f
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-05-29 11:37:07 +0300

    added keymap to toggle copilot, i got mad writing the command every time

--- lua/jaysh/lazy/init.lua
+++ lua/jaysh/lazy/init.lua
@@ -1,5 +1,13 @@
 return {
         "github/copilot.vim",
+        vim.keymap.set('n', "<leader>c",
+            function()
+                if vim.b.copilot_enabled then
+                    vim.cmd("Copilot disable")
+                else
+                    vim.cmd("Copilot enable")
+                end
+            end),
         {
             "nvim-lua/plenary.nvim",
             name = "plenary"
