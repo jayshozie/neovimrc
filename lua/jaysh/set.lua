868333d1158341ba5a4b7f1672f6e65dedc86c7c
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-06-10 20:28:24 +0300

    need test

--- lua/jaysh/set.lua
+++ lua/jaysh/set.lua
@@ -28,6 +28,6 @@ vim.opt.updatetime = 50
 
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 70
+vim.g.netrw_winsize = 100
 vim.opt.splitright = true
 vim.opt.splitbelow = true
