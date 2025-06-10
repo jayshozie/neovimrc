9f322a6455703f305488392a8078d3932663595e
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-06-10 20:28:48 +0300

    nvm

--- lua/jaysh/set.lua
+++ lua/jaysh/set.lua
@@ -28,6 +28,6 @@ vim.opt.updatetime = 50
 
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 100
+vim.g.netrw_winsize = 50
 vim.opt.splitright = true
 vim.opt.splitbelow = true
